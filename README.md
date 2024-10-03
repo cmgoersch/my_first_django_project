9fb761970d2e4cea0b8e86e7d3f656f73a3fb901
cmgoersch <goersch@madways.de>
2024-10-03 21:36:55 +0200

    Database connected

--- README.md
+++ README.md
@@ -6,3 +6,12 @@ zum Starten brauchst du nur
 
 eingeben.
 
+Sehen kannst du die Liste dann bei dir lokal auf:
+
+http://127.0.0.1:8000/mylist/
+oder http://localhost:8000/mylist/
+
+Gebaut habe ich das größtenteils nach der Anleitung aus diesem Tutorial
+https://www.youtube.com/watch?v=Tl0RsoMMSic
+
+
--- mylist/views.py
+++ mylist/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from .models import ShoppingItem
 
 # Create your views here.
 def mylist(request):
+    if request.method == 'POST':
+        print('Recived data:', request.POST['itemName'])
+        ShoppingItem.objects.create(name = request.POST['itemName'])
     return render(request, 'shopping_list.html')
