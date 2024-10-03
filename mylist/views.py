29ca7fb2d368f05b068d9fadfd969aec1246f230
cmgoersch <goersch@madways.de>
2024-10-03 21:49:21 +0200

    frontend connected

--- mylist/views.py
+++ mylist/views.py
@@ -6,4 +6,5 @@ def mylist(request):
     if request.method == 'POST':
         print('Recived data:', request.POST['itemName'])
         ShoppingItem.objects.create(name = request.POST['itemName'])
-    return render(request, 'shopping_list.html')
+    all_items = ShoppingItem.objects.all()
+    return render(request, 'shopping_list.html', {'all_items': all_items})
